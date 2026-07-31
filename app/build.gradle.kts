80539cc28bd461853168cf86e3b01ba4e696584b
Alexander Melnichenko <clychnik@gmail.com>
2026-07-31 10:59:58 +0300

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.kotlin.ksp)
+}
+
+android {
+    namespace = "ru.netology.nework"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "ru.netology.nework"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
+    implementation(libs.androidx.room) // Room (для базы данных)
+    ksp(libs.androidx.room.compiler) //Для компиляции базы данных
+    implementation(libs.hilt.android) // Hilt (для внедрения зависимостей)
+    ksp(libs.hilt.android.compiler) // Hilt (для внедрения зависимостей)
+    implementation(libs.androidx.lifecycle.livedata.ktx) // Coroutines (для работы с асинхронностью)
+    implementation(libs.androidx.fragment) // Для viewModel в рагментах
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
+
+}
