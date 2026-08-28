8e8b2e1df2013c553304da4c6536199d57961f1f
Alexander Melnichenko <clychnik@gmail.com>
2026-08-28 21:02:05 +0300

    Job data class

---
+++ app/src/main/java/ru/netology/nework/data/dto/job/JobItem.kt
@@ -0,0 +1,10 @@
+package ru.netology.nework.data.dto.job
+
+data class JobItem(
+    val finish: String,
+    val id: Int,
+    val link: String,
+    val name: String,
+    val position: String,
+    val start: String
+)
