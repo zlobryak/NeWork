213f76b9332e0e1582bb9a499cef20806f16b53f
Alexander Melnichenko <clychnik@gmail.com>
2026-09-09 16:33:06 +0300

    Events fragments and nav

--- app/src/main/java/ru/netology/nework/ui/fragments/events/EventDetailFragment.kt
+++ app/src/main/java/ru/netology/nework/ui/fragments/events/EventDetailFragment.kt
@@ -1,7 +1,7 @@
-package ru.netology.nework.ui.fragments
+package ru.netology.nework.ui.fragments.events
 
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class UsersFragment : Fragment() {}
+class EventDetailFragment : Fragment()
